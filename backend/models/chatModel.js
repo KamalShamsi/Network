f14de47d858c64ac95f4963c46f5ea1bd83569b5
KamalShamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-03 04:25:36 -0700

    Comments added to chatModel

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,34 +1,43 @@
-const mongoose = require('mongoose')
+// Importing the mongoose library
+const mongoose = require('mongoose');
 
+// Creating a new schema for chat
 const chatModel = mongoose.Schema(
     {
-        chatName: { type: String, trim: true},
-        isGroupChat: {type: Boolean, default: false},
-    users: [
-        {
-            type:mongoose.Schema.Types.ObjectId,
-            ref:"User",
+        // Name of the chat (can be a group name or individual's name)
+        chatName: { type: String, trim: true },
+
+        // Boolean to check if the chat is a group chat. Default is set to false.
+        isGroupChat: { type: Boolean, default: false },
+
+        // Array of user IDs associated with the chat
+        users: [
+            {
+                type: mongoose.Schema.Types.ObjectId, // Reference to User's ObjectId
+                ref: "User",                          // Reference to the User model
+            },
+        ],
+
+        // Reference to the latest message in the chat
+        latestMessage: {
+            type: mongoose.Schema.Types.ObjectId,   // Reference to Message's ObjectId
+            ref: "Message",                         // Reference to the Message model
         },
-    ],
-    latestMessage: {
-        type: moongoose.Schema.Types.ObjectId,
-        ref: "Message",
-    },
-    groupAdmin: {
-        type: moongoose.Schema.Types.ObjectId,
-        ref: "User",
+
+        // In case of a group chat, reference to the admin of the group
+        groupAdmin: {
+            type: mongoose.Schema.Types.ObjectId,   // Reference to User's ObjectId
+            ref: "User",                           // Reference to the User model
         },
     }, 
     {
-        timestams: true,
+        // Enable timestamps (createdAt and updatedAt fields)
+        timestamps: true,
     }
-    );
+);
 
+// Creating a new Chat model using the chat schema
 const Chat = mongoose.model("Chat", chatModel);
 
+// Exporting the Chat model for use in other parts of the application
 module.exports = Chat;
-//chatName
-//isGroupChat
-//users
-//latestMessage
-//groupAdmin 

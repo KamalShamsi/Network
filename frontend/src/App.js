8364c1b798f97d3fb42f9b75cab2f999da652107
KamalShamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-03 02:12:45 -0700

    Testing Chakra UI stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
+import { Spinner } from "@chakra-ui/spinner"
 
 function App() {
-  return <div className="App">Home page</div>
+  return <div className="App">
+      <Spinner size='xs' />
+      <Spinner size='sm' />
+      <Spinner size='md' />
+      <Spinner size='lg' />
+      <Spinner size='xl' />
+  </div>
 }
 export default App;

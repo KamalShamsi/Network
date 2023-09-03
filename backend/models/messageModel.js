8eedc185acecc5b822713a3baed3497981106bd4
KamalShamsi <Kamaluddin.Shamsi@gmail.com>
2023-09-03 04:27:17 -0700

    comments added to messageModel

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,15 +1,24 @@
-const mongoose = require('mongoose')
+// Importing the mongoose library
+const mongoose = require('mongoose');
 
+// Creating a new schema for message
 const messageModel = mongoose.Schema({
-    sender :{type: mongoose.Schema.Types.ObjectId, ref: "User"},
-    content :{type: String, trim: True},
-    chat: {type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
-    },
-    {
-        timestamps: true,
-    }
-    );
+    // Reference to the sender's ID from the User model
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 
-    const Message = mongoose.model("Message", messageModel);
+    // Content of the message with leading and trailing whitespace removed
+    content: { type: String, trim: true },
 
-    module.exports = Message; 
+    // Reference to the chat's ID the message belongs to from the Chat model
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+},
+{
+    // Enable timestamps (createdAt and updatedAt fields)
+    timestamps: true,
+});
+
+// Creating a new Message model using the message schema
+const Message = mongoose.model("Message", messageModel);
+
+// Exporting the Message model for use in other parts of the application
+module.exports = Message;

459ed4e9893d6237ab9e7765fc3a61e9d225fc37
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-09-04 12:22:36 -0700

    Fixed React dependency issues and updated code for React 18

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = document.getElementById("root");
+const reactRoot = createRoot(root);
+
+reactRoot.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ChakraProvider>
-    <App />
-    </ChakraProvider>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );

d28a15665dd439084a87fc332fb3379b77aa659a
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-09-04 12:26:22 -0700

    App added comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
+// Importing the main stylesheet for the app
 import './App.css';
-import { Route } from "react-router-dom";
-import  HomePage from "./Pages/HomePage";
-import  ChatPage from "./Pages/ChatPage";
 
+// Importing necessary components from react-router-dom for routing
+import { Route } from "react-router-dom";
+
+// Importing page components
+import HomePage from "./Pages/HomePage";
+import ChatPage from "./Pages/ChatPage";
+
+// Main App component
 function App() {
-  return <div className="App">
-    <Route path="/" component={HomePage} exact/>
-    <Route path="/chats" component={ChatPage}/>
-  </div>
+  return (
+    <div className="App">
+      {/* Defining a route for the home page. "exact" ensures this route is matched only if the path is exactly "/". */}
+      <Route path="/" component={HomePage} exact />
+      
+      {/* Defining a route for the chat page. */}
+      <Route path="/chats" component={ChatPage} />
+    </div>
+  );
 }
+
+// Exporting the App component to be used in other parts of the app
 export default App;

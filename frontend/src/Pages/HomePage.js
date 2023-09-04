df8732029dce50bda0e7cab7a942e0469f031ead
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-09-04 15:40:42 -0700

    Login Page components added

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,13 +1,67 @@
-import React from 'react';
+// Import necessary components from Chakra UI
+import {
+    Box,
+    Container,
+    Tab,
+    TabList,
+    TabPanel,
+    TabPanels,
+    Tabs,
+    Text,
+    Flex
+} from "@chakra-ui/react";
 
-// Functional component representing the HomePage.
-const HomePage = () => {
+/*import Login from "../Components/Authentication/Login";
+import SignUp from "../Components/Authentication/SignUp";
+*/
+
+// Define the Homepage functional component
+function Homepage() { 
     return (
-        <div>
+        // Main container for the homepage with a max width and content centered
+        <Container maxW="xl" centerContent>
+        
+            {/* Flex box used to center the 'Network' text both horizontally and vertically */}
+            <Flex
+                justifyContent="center"
+                alignItems="center"
+                p={3}
+                bg="white"
+                w="100%"
+                m="40px 0 15px 0"
+                borderRadius="lg"
+                borderWidth="1px"
+                height="100px" 
+            >
+                <Text fontSize="4xl" fontFamily="Work sans">
+                    Network
+                </Text>
+            </Flex>
             
-        </div>
-    )
+            {/* Box containing the Login and Sign Up tabs */}
+            <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+                <Tabs isFitted variant="soft-rounded">
+                    {/* Tab titles */}
+                    <TabList mb="1em">
+                        <Tab>Login</Tab>
+                        <Tab>Sign Up</Tab>
+                    </TabList>
+                    {/* Tab content */}
+                    <TabPanels>
+                        {/* Login tab content */}
+                        <TabPanel>
+                            <div>Login components...</div>
+                        </TabPanel>
+                        {/* Sign Up tab content */}
+                        <TabPanel>
+                            <div>Sign Up components...</div>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+        </Container>
+    );
 }
 
-// Exporting HomePage for use in other components.
-export default HomePage;
+// Export the Homepage component for use in other parts of the app
+export default Homepage;

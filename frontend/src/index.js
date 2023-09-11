6af59a5d4ac94b4ef67c2c79a4ef63874f0e50cd
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-09-10 23:12:08 -0700

    reportWebVitals created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
 import { BrowserRouter } from "react-router-dom";
 
-// Getting root HTML element for React app.
-const root = document.getElementById("root");
-
-// Initializing React concurrent mode.
-const reactRoot = createRoot(root);
-
-// Rendering the main App with routing and Chakra UI support.
-reactRoot.render(
-  <React.StrictMode>
+ReactDOM.render(
+  <ChakraProvider>
     <BrowserRouter>
-      <ChakraProvider>
+      <ChatProvider>
         <App />
-      </ChakraProvider>
+      </ChatProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

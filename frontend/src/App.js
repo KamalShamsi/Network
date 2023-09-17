7d14b3a620ebe856c76dad0326a0d69ea03786aa
KamalShamsi <kamaluddin.shamsi@gmail.com>
2023-09-17 16:52:50 -0700

    Fixed errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,26 @@
 import './App.css';
 
 // Importing necessary components from react-router-dom for routing
-import { Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 // Importing page components
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
 import { ChakraProvider } from "@chakra-ui/react";
 
-
 // Main App component
 function App() {
   return (
     <ChakraProvider>
-    <div className="App">
-      {/* Defining a route for the home page. "exact" ensures this route is matched only if the path is exactly "/". */}
-      <Route path="/" component={HomePage} exact />
-      
-      {/* Defining a route for the chat page. */}
-      <Route path="/chats" component={ChatPage} />
-    </div>
+      <div className="App">
+        <Routes>
+          {/* Defining a route for the home page. "path" ensures this route is matched only if the path is exactly "/". */}
+          <Route path="/" element={<HomePage />} />
+          
+          {/* Defining a route for the chat page. */}
+          <Route path="/chats" element={<ChatPage />} />
+        </Routes>
+      </div>
     </ChakraProvider>
   );
 }
